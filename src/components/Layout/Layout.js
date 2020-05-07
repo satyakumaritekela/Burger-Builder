447c8481e79033ee2511c7cdf9534c7809fa2348
Satya Kumar Itekela <st798799@dal.ca>
2020-05-07 20:19:36 -0300

    changes in react-scripts

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import classes from "./Layout.module.css";
+
+const layout = (props) => (
+  <Fragment>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.ToolBar_Margin}>{props.children}</main>
+  </Fragment>
+);
+
+export default layout;
